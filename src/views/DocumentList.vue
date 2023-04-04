10ed4627cc626ddd2237979490fbcb9a00fbd55c
John Vincent Verzosa <johnvincent.versoza@journeytech.com.phm>
2023-04-04 17:09:41 +0800

    added name

--- src/views/DocumentList.vue
+++ src/views/DocumentList.vue
@@ -212,6 +212,7 @@ export default {
   },
   methods: {
     submitButton() {
+      const myName = "My name is Hihuhihuhohu";
       this.resultDialog = false;
       const sumProd = this.prod1Val + this.prod4Val;
       this.sumProduct = sumProd;
